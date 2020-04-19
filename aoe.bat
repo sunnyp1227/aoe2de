fa18f2196d827eca5a91c524d6f8b04bc18fbf42
Sunny Peng <sunnyp1227@gmail.com>
2020-04-18 18:32:42 -0700

    fix bat comment

--- aoe.bat
+++ aoe.bat
@@ -1,8 +1,8 @@
 @echo off
 set STEAM_ID=76561198001071301
 
-# List of supported languages: 
-# en, de, el, es, es-MX, fr, hi, it, ja, ko, ms, nl, pt, ru, tr, vi, zh, zh-TW
+:: List of supported languages: 
+:: en, de, el, es, es-MX, fr, hi, it, ja, ko, ms, nl, pt, ru, tr, vi, zh, zh-TW
 set LANGUAGE=en
 
 python aoe.py %STEAM_ID% %LANGUAGE%
