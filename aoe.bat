d94ace60e5c22024218452de6a295c0b4305756e
Sunny Peng <sunnyp1227@gmail.com>
2020-04-18 18:29:05 -0700

    fix map display language bug and add multi-lang support

--- aoe.bat
+++ aoe.bat
@@ -1,3 +1,8 @@
 @echo off
 set STEAM_ID=76561198001071301
-python aoe.py %STEAM_ID%
+
+# List of supported languages: 
+# en, de, el, es, es-MX, fr, hi, it, ja, ko, ms, nl, pt, ru, tr, vi, zh, zh-TW
+set LANGUAGE=en
+
+python aoe.py %STEAM_ID% %LANGUAGE%
